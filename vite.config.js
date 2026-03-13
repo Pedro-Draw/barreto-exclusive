719ffb7309b3a50b502aee67aa9a004ee05c4959
Pedro-Draw <pedrinsouzaa73@gmail.com>
2026-03-13 09:04:08 -0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
-import { defineConfig } from "vite"
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'  // ← agora vai resolver depois de instalar
 
 export default defineConfig({
+  plugins: [react()],
+  base: '/barreto-exclusive/',           // ← importante pro GitHub Pages
   server: {
     port: 5173,
     open: true
